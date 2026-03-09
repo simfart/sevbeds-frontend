b80a847233a6720c940ac14cc29c0b2f4d43cdaa
Art <simf.i@ya.ru>
2026-03-09 13:33:56 +0300

    title changed

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styles from './HomePage.module.scss';
+import React from "react";
+import styles from "./HomePage.module.scss";
 
 export function HomePage() {
   return (
     <div className={styles.hero}>
-      <h1 className={styles.title}>Аренда медицинских кроватей</h1>
+      <h1 className={styles.title}>Аренда мед кроватей</h1>
       <p className={styles.subtitle}>
-        Удобные и надёжные кровати для ухода за больными на дому и в медучреждениях.
+        Удобные кровати для ухода за больными на дому и в медучреждениях.
       </p>
       <a href="/contacts" className={styles.cta}>
         Связаться с нами
